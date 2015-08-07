4930b3924f26535876e2d04e607b867cf7930350
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-06 22:34:16 -0400

    Adds dependency to event confirmations for destroy method.

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :created_by, inverse_of: :events, class_name: User, :foreign_key => 'user_id'
 
   has_many :users, through: :confirmations
-  has_many :confirmations
+  has_many :confirmations, dependent: :destroy
 
   def confirmation_count
     confirmations.count
